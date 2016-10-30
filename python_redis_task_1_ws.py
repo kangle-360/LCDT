907f94c3f2209fac4c46f15963446cdf62d4547b
WangShuai0413 <WangShuai0413@users.noreply.github.com>
2016-10-30 15:55:02 +0800

    Update python_redis_task_1_ws.py

--- python_redis_task_1_ws.py
+++ python_redis_task_1_ws.py
@@ -1,19 +1,19 @@
 # -*- coding:utf-8 -*-
 import redis
 
-r=redis.Redis(host="127.0.0.1",port=6379,db=0)
+r=redis.Redis ( host = "127.0.0.1" , port = 6379,db = 0)
 
-def setValueInRedis(key,value):
-    if key==None or value==None:
+def setValueInRedis ( key , value ) :
+    if key == None or value == None :
         return None
-    elif r.set(key,value):
+    elif r.set ( key,value ) :
         return 1
-    else: 
+    else : 
         return 0
     
-def getValueInRedis(key):
-    if key:
-        value=r.get(key)
-        if value:
+def getValueInRedis ( key ) :
+    if key :
+        value = r.get ( key )
+        if value :
             return value
     return None
